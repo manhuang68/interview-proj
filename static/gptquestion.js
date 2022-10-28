6871577af32df6a92f561ce58179a28878446b32
lipinghu-cu <liping.hu@enertis.com>
2022-10-28 19:27:19 -0400

    return added on gptquestion page

--- static/gptquestion.js
+++ static/gptquestion.js
@@ -1,34 +1,52 @@
 $(document).ready(function () {
-    if(["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"].includes(position[0])) {
-        $("#listtitle").html("Here is the most asked multi-part question related to " + topic + " for an " + position + " interview: ")
-    }
-    else {
-        $("#listtitle").html("Here is the most asked multi-part question related to " + topic + " for a " + position + " interview: ")
-    }
-    $('#quest').append("<div>"+question+"</div>")
+
+
+
+    $('#keywords').keypress(function (e) {
+        if (e.which == 13) {
+            submit()
+        }
+    });
+
 
     $("#q3").click(function(e) {
-        console.log("hello");
-        $(".load").html("<img src = 'https://media4.giphy.com/media/zlcIBNopQj8Yx5QgpR/giphy.gif'>")
         e.preventDefault()
-        keys = $('#keywords').val().trim()
-        $.ajax({
-            type : "POST",
-            url : '/question3',
-            dataType: "json",
-            data: JSON.stringify([keys]),
-            contentType: 'application/json;charset=UTF-8',
-            success: function(result) {
-                console.log(result['res']);
-                window.location.replace("/responsepage");
-
-            },
-            error: function(request, status, error) {
-                console.log("Error")
-                console.log(request)
-                console.log(status)
-                console.log(error)
-            }
-        });
+        submit()
     })
+
+
+    function submit() {
+
+        if(["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"].includes(position[0])) {
+            $("#listtitle").html("Here is the most asked multi-part question related to " + topic + " for an " + position + " interview: ")
+        }
+        else {
+            $("#listtitle").html("Here is the most asked multi-part question related to " + topic + " for a " + position + " interview: ")
+        }
+        $('#quest').append("<div>"+question+"</div>")
+    
+            console.log("hello");
+            $(".load").html("<img src = 'https://media4.giphy.com/media/zlcIBNopQj8Yx5QgpR/giphy.gif'>")
+            keys = $('#keywords').val().trim()
+            $.ajax({
+                type : "POST",
+                url : '/question3',
+                dataType: "json",
+                data: JSON.stringify([keys]),
+                contentType: 'application/json;charset=UTF-8',
+                success: function(result) {
+                    console.log(result['res']);
+                    window.location.replace("/responsepage");
+    
+                },
+                error: function(request, status, error) {
+                    console.log("Error")
+                    console.log(request)
+                    console.log(status)
+                    console.log(error)
+                }
+            });
+        
+    } //end submit function
+
 });
