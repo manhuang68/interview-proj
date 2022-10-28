52e8e25c5c6d437b6b63bd320f0437069c293b85
Erik Hansen <erikhansen@dyn-160-39-148-153.dyn.columbia.edu>
2022-10-28 17:43:24 -0400

    Cleaning up code, adding a picture, making the GPT prompts more thorough

--- server.py
+++ server.py
@@ -2,9 +2,8 @@ import os
 from flask import Flask
 from flask import render_template
 from flask import Response, request, jsonify
-from firebase_admin import credentials, firestore, initialize_app
 import openai
-openai.api_key = "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"
+openai.api_key = "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"
 
 # Initialize Flask app
 app = Flask(__name__)
@@ -26,6 +25,24 @@ def welcome():
 def start():
     return render_template('start.html')
 
+@app.route('/topics')
+def topics():
+    return render_template('topics.html', topics=topics_list)
+
+@app.route('/gptquestion')
+def gptquestion():
+    return render_template('gptquestion.html', question=question)
+
+@app.route('/responsepage')
+def responsepage():
+    return render_template('responsepage.html', responsetoq=response)
+
+@app.route('/editresponsepage')
+def editresponsepage():
+    return render_template('editresponsepage.html', editresponse=editresp)
+
+
+# AJAX FUNCTIONS
 @app.route('/question1', methods=['GET', 'POST'])
 def q1():
     global position
@@ -41,10 +58,6 @@ def q1():
         topics_list.append(line)
     return jsonify(topics=result)
 
-@app.route('/topics')
-def topics():
-    return render_template('topics.html', topics=topics_list)
-
 @app.route('/question2', methods=['GET', 'POST'])
 def q2():
     global top
@@ -52,14 +65,10 @@ def q2():
     global position
     data = request.get_json()
     top = data[0]
-    prompt = "Give only 1 most asked interview question about "+ top +" at a "+ position +" interview"
+    prompt = "Give 1 multi-part interview question about "+ top +" at a "+ position +" interview. Make the question detailed."
     completion = openai.Completion.create(engine = "text-davinci-002", max_tokens = 256, prompt = prompt)
     question = completion.choices[0].text.strip()
     return jsonify(q=question)
-
-@app.route('/gptquestion')
-def gptquestion():
-    return render_template('gptquestion.html', question=question)
 
 @app.route('/question3', methods=['GET', 'POST'])
 def q3():
@@ -68,14 +77,10 @@ def q3():
     global response
     data = request.get_json()
     keyws = data[0]
-    prompt = "Give a 5 sentence answer to this question: " + question + " using the following keywords/phrases: " + keyws + "."
+    prompt = "Give a 5 sentence answer to this question: " + question + " using the following keywords/phrases: " + keyws + ". Make sure the answer is detailed and fully responds to the given question."
     completion = openai.Completion.create(engine = "text-davinci-002", max_tokens = 256, prompt = prompt)
     response = completion.choices[0].text.strip()
     return jsonify(res=response)
-
-@app.route('/responsepage')
-def responsepage():
-    return render_template('responsepage.html', responsetoq=response)
 
 @app.route('/question4', methods=['GET', 'POST'])
 def q4():
@@ -84,63 +89,10 @@ def q4():
     global editresp
     data = request.get_json()
     editkeyws = data[0]
-    prompt = "Can you make a 7 sentence responce to this question: " + question + ". An example response to this question is this: " + response + ". Can you improve upon this response by making it longer and including the following keywords/phrases: " + editkeyws + "."
+    prompt = "Can you make a more detailed responce to this question: " + question + ". An example response to this question is this: " + response + ". Can you improve upon this response by making it longer and including the following keywords/phrases: " + editkeyws + "."
     completion = openai.Completion.create(engine = "text-davinci-002", max_tokens = 256, prompt = prompt)
     editresp = completion.choices[0].text.strip()
     return jsonify(editres=editresp)
-
-@app.route('/editresponsepage')
-def editresponsepage():
-    return render_template('editresponsepage.html', editresponse=editresp)
-
-@app.route('/add_rsvp/<id>', methods=['GET', 'POST'])
-def add_rsvp(id=0):
-    try:
-        rsvp_id = request.json['id']
-        current_party_ref = db.collection('party info').document(id)
-        rsvp_ref = current_party_ref.collection('RSVPs')
-        rsvp_ref.document(rsvp_id).set(request.json)
-        return jsonify({"success": True}), 200
-    except Exception as e:
-        return f"An Error Occurred: {e}"
-
-
-@app.route('/view/<id>')
-def view(id=0):
-    return render_template('party.html', item=id)
-
-@app.route('/add_data')
-def add_data():
-    return render_template('add.html')
-
-@app.route('/add_party', methods=['GET', 'POST'])
-def add_party():
-    try:
-        id = request.json['id']
-        party_ref.document(id).set(request.json)
-        return jsonify({"success": True, "new_id": id}), 200
-    except Exception as e:
-        return f"An Error Occurred: {e}"
-
-
-@app.route('/edit/<id>')
-def edit(id=0):
-    global data
-
-    return render_template('edit.html', item=str(int(id)-1))
-
-@app.route('/edit/save_entry', methods=['GET', 'POST'])
-def save_entry():
-    try:
-        id = request.json['id']
-        party_ref.document(id).set(request.json)
-        return jsonify({"success": True}), 200
-    except Exception as e:
-        return f"An Error Occurred: {e}"
-
-
-# AJAX FUNCTIONS
-
 
 if __name__ == '__main__':
     app.run(debug=True)
